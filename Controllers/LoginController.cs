49333fe2bedd2b6cdb769746bd99d0d86d09ac9e
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-03-20 15:41:07 -0300

    feat: add jwtoken

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,16 +1,20 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 
 [ApiController]
 [Route("v1/usuario/login")]
 public class LoginController : ControllerBase
 {
-    // Supondo que você tenha um serviço de autenticação injetado
     private readonly IAutenticacaoService _autenticacaoService;
+    private readonly IConfiguration _appSettings;
 
-    public LoginController(IAutenticacaoService autenticacaoService)
+    public LoginController(IAutenticacaoService autenticacaoService, IConfiguration appSettings)
     {
         _autenticacaoService = autenticacaoService;
+        _appSettings = appSettings;
     }
 
     [HttpPost]
@@ -23,8 +27,29 @@ public class LoginController : ControllerBase
             return Unauthorized("CPF e/ou senha incorretos.");
         }
 
-        // lembrar de gerar um token JWT pra retornar na resposta
+        var token = CreateToken(usuario);
 
-        return Ok(new { message = "Login bem-sucedido." });
+        return Ok(new { message = "Login bem-sucedido.", token = token });
+    }
+
+
+    private string CreateToken(Usuario usuario)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_appSettings["Jwt:Key"]);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, usuario.Id.ToString()),
+            }),
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            Issuer = _appSettings["Jwt:Issuer"],
+            Audience = _appSettings["Jwt:Audience"]
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();   // <<<<
+builder.Services.AddControllers();   // controller
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,6 +18,28 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // experimentando adicionar serviço de autenticação
 builder.Services.AddScoped<IAutenticacaoService, AutenticacaoService>();
 
+// experimentando adicionar jwt
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -24,5 +49,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.MapControllers();   // <<<<
+app.UseAuthentication(); // jwt
+app.UseAuthorization(); // jwt
+app.MapControllers();   // controller
 app.Run();
